a19b8361c7443c8c5da6d25f3544ca3d71e31fbb
Andrey-Diomov <andreydiomov@gmail.com>
2020-02-05 23:14:37 +0300

    push

---
+++ src/diomov/unit1_basics/Task1_4.java
@@ -0,0 +1,18 @@
+package diomov.unit1_basics;
+
+import java.util.Scanner;
+
+public class Task1_4 {
+    public static void main(String[] args) {
+        double number;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("number = ");
+        number = input.nextDouble();
+
+        double left = Math.floor(number);
+        double right = (number - left) * 1000;
+        double inverted = right + left / 1000;
+        System.out.printf("x= %.3f, inverted = %.3f", number, inverted);
+    }
+}
---
+++ src/diomov/unit1_basics/Task1_5.java
@@ -0,0 +1,19 @@
+package diomov.unit1_basics;
+
+import java.util.Scanner;
+
+public class Task1_5 {
+    public static void main(String[] args) {
+        int timeInSeconds;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        timeInSeconds = input.nextInt();
+
+        int hours = timeInSeconds / 3600;
+        int minutes = (timeInSeconds / 60) % 60;
+        int seconds = timeInSeconds - hours * 3600 - minutes * 60;
+
+        System.out.printf("%02dч %02dмин %02dс\n", hours, minutes, seconds);
+    }
+}
